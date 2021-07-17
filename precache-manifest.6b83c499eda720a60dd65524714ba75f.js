67ed6d7a6657812e5ae94526df8a19956954f204
Salman <slmnshq25@gmail.com>
2021-07-17 22:58:10 +0530

    initial commit

---
+++ precache-manifest.6b83c499eda720a60dd65524714ba75f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee789a7ed0e076608a3eb75fa525399",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bb9de77d87efabd47137",
+    "url": "./static/css/main.16d87580.chunk.css"
+  },
+  {
+    "revision": "2e4809e4f4c1508b443d",
+    "url": "./static/js/2.51b75e4c.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "./static/js/2.51b75e4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb9de77d87efabd47137",
+    "url": "./static/js/main.87d056fc.chunk.js"
+  },
+  {
+    "revision": "62dfc1b45aeaac6ee56b",
+    "url": "./static/js/runtime-main.d28a0c19.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "./static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "./static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "7823cdc7152c122161c46fecaa200305",
+    "url": "./static/media/Untitled2.7823cdc7.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "./static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "./static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "./static/media/manOnTable.52be3144.svg"
+  }
+]);
